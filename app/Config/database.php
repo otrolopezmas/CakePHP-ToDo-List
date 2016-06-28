eb5582a7fa8a7f5f2aa51b21f63a592aeeca2749
Marcos <lopezcmarcos@gmail.com>
2016-06-28 11:48:37 +0200

    First upload
    CakePHP app folder

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'todoapp',
+		'password' => 'y45DdKbJsBrKUAaM',
+		'database' => 'todoapp',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
